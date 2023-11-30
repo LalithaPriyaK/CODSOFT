7e07805960d761d53338e4a681d621cebafd16e2
Lalitha priya krishnan <136954112+LalithaPriyaK@users.noreply.github.com>
2023-11-30 20:49:45 +0530

    Delete Downloadresume.js

--- Downloadresume.js
+++
@@ -1,8 +0,0 @@
-window.jsPDF = window.jspdf.jsPDF;
-const res=document.getElementById('main5-button');
-res.addEventListener('click', function() {
-
-    const g = 'https://drive.google.com/file/d/1HoK9MIPyQMw991gX0DOqRpntd7DlGsqa/view?usp=drive_link';
-    window.open(g, '_blank');
-
-});
